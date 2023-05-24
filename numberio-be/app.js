d408e0dd5d915cef89a42927a1f782744c8dea9b
Văn Thế Vinh <vtvinh1412@gmail.com>
2023-05-24 09:14:26 +0700

    load-blancing, docker

--- numberio-be/app.js
+++ numberio-be/app.js
@@ -1,53 +1,60 @@
-const express = require("express");
-const moongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, MONGO_DB_URL } = require('./utils/config');
+
 const app = express();
-const accountRouter = require("./routes/accountRouter");
-const questionRoute = require("./routes/questionRouter");
-const signinRouter = require("./routes/signin.route");
-const gameRouter = require("./routes/gameRouter");
-const examRouter = require("./routes/examRouter");
+const accountRouter = require('./routes/accountRouter');
+const questionRoute = require('./routes/questionRouter');
+const signinRouter = require('./routes/signin.route');
+const gameRouter = require('./routes/gameRouter');
+const examRouter = require('./routes/examRouter');
 //connect database
-const connectDB = async () => {
-  try {
-    await moongoose.connect(
-      "mongodb+srv://shinobihao2001:4aqZ56MycMDOtk08@numberiobe.tfoooql.mongodb.net/?retryWrites=true&w=majority",
-    );
-    console.log("Connect DB successfully");
-  } catch (error) {
-    console.log("Mongoose Error:", error);
-    process.exit(1);
-  }
-};
-connectDB();
-moongoose.set("strictQuery", false);
+// const connectDB = async () => {
+//   try {
+//     await moongoose.connect(MONGO_DB_URL);
+//     console.log('Connect DB successfully');
+//   } catch (error) {
+//     console.log('Mongoose Error:', error);
+//     process.exit(1);
+//   }
+// };
+// connectDB();
+// moongoose.set('strictQuery', false);
+
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 ///////////////////////
 
 ///////
 //temporary router index
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   // res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-app.use("/question", questionRoute);
+app.use('/question', questionRoute);
 ///
-app.use("/signin", signinRouter);
-app.use("/account", accountRouter);
-app.use("/game", gameRouter);
-app.use("/exam", examRouter);
-app.get("/", (req, res) => {
-  res.send("Hello world 22");
+app.use('/signin', signinRouter);
+app.use('/account', accountRouter);
+app.use('/game', gameRouter);
+app.use('/exam', examRouter);
+
+app.get('/', (req, res) => {
+  res.send(`Hello world ${PORT}`);
 });
-const port = 4000;
-app.listen(process.env.PORT || port, () => {
-  console.log(
-    `Numberio is runing on  http://localhost:${process.env.PORT || port}`
-  );
-});
+
+module.exports = app;
--- numberio-be/utils/config.js
+++ numberio-be/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
 
 module.exports = {
   PORT,
+  MONGO_DB_URL,
 };
