c88d8c7d57a5544a3ee1f227b97835d907e7f460
letiendat200 <91449204+letiendat200@users.noreply.github.com>
2023-06-11 14:29:38 +0700

    Update index.js

--- numberio-fe/src/pages/index.js
+++ numberio-fe/src/pages/index.js
@@ -2,10 +2,12 @@ import NotFound from "./notfound";
 import SignIn from "./signin";
 import LearningPage from "./learning";
 import HomePage from "./home";
+import ProfilePage from "./profile/ProfilePage";
 
 export {
     NotFound,
     SignIn,
     LearningPage,
     HomePage,
+    ProfilePage,
 }
