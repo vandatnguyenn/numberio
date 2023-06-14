eb76c0fc18e6357141a521f83917f9ceaf73129d
Ha Bao Khang <19120252@student.hcmus.edu.vn>
2023-06-14 19:55:27 +0700

    add path

--- numberio-fe/src/App.js
+++ numberio-fe/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { NotFound, LearningPage, HomePage, ProfilePage } from "./pages";
 import { ProtectedLayout } from "./components/layout";
+import AdminPage from "./pages/adminPage/adminPage"
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Route element={<ProtectedLayout />}>
         <Route path="/" element={<HomePage/>} />
         <Route path="learning" element={<LearningPage/>} />
-       
+        <Route path="adminPage" element={<AdminPage/>} />
         <Route path="*" element={<NotFound />} />
         <Route path="profilePage" element={<ProfilePage/>} />
       </Route>
